99ee8dc4b91592f3c51dd33aacd421201bd6a534
wu <84631500+Wu-haifen@users.noreply.github.com>
2022-09-04 09:58:36 +0800

    完成头部标签路由跳转功能 (#4)
    Co-authored-by: WU Haifen <haifen.wu@hexagon.com>

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="relative bg-white">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-center text-center">
-      <h1 class="text-3xl tracking-tight font-bold text-gray-900">首页</h1>
-    </div>
+      <h1 class="text-3xl tracking-tight font-bold text-gray-900"><slot name="title"></slot></h1>
+    </div> 
   </header>
 </template>
 
--- src/views/TakeNotesTab/index.vue
+++ src/views/TakeNotesTab/index.vue
@@ -1,12 +1,34 @@
 <template>
-  <NavBarVue></NavBarVue>
-  <div>
-    随心笔记
+  <div class="min-h-full">
+    <!-- 消息通知 -->
+    <NavBarMsg/>
+    <!--导航栏-->
+    <NavBar/>
+    <!-- 头部标题 -->
+    <Header>
+      <template v-slot:title>
+        随心笔记
+      </template>
+    </Header>
+    <!-- 主题部分内容 -->
+    <!-- <Main></Main> -->
+    <LeftMain/>
+    
+    <!--底部-->
+    <Footer></Footer>
+    
   </div>
 </template>
 
 <script setup lang="ts">
-import NavBarVue from '@/components/NavBar/NavBar.vue';
+import NavBar from '@/components/NavBar/NavBar.vue';
+import NavBarMsg from '@/components/NavBar/NavBarMsg.vue';
+import Header from '@/components/Header/Header.vue';
+import Main from '@/components/Main/Main.vue';
+import LeftMain from '@/components/Main/LeftMain/LeftMain.vue';
+import Footer from '@/components/Footer/Footer.vue';
+// import { defineComponent } from '@vue/runtime-core';
+// export default defineComponent({name:'NavBar',component:{NavBarVue}})
 </script>
 
 <style scoped>
